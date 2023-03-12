380a08aebc5f0c50883f6112d79868c93d58cd3c
Arnau Mir <arnau.mir@estudiantat.upc.edu>
2023-03-12 20:29:04 +0100

    Correction

--- src/test/electron/messageClassifier/MessageClassifierTest.test.ts
+++ src/test/electron/messageClassifier/MessageClassifierTest.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals';
 import { FileManager } from "../../../electron/utils/FileManager";
 
-test('givenValidDataFile_WhenClssifyMessages_thenNotNull', async () => {
+test('givenValidDataFile_WhenClassifyMessages_thenNotNull', async () => {
     // Given
     var fileManager : FileManager = new FileManager();
 
