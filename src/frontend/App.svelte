1f284a2034f63d186b953c9fa47d7db310013429
Marc Azlor <marc.azlor@estudiantat.upc.edu>
2023-05-17 19:01:37 +0200

    general style

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -8,6 +8,132 @@
     width: 100%;
     height: 100%;
   }
+  .inner {
+    max-width: 980px;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin: auto;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+
+  .mobile-icon {
+    width: 25px;
+    height: 14px;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .mobile-icon:after,
+  .mobile-icon:before,
+  .middle-line {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: #fff;
+    transition: all 0.4s;
+    transform-origin: center;
+  }
+
+  .mobile-icon:before,
+  .middle-line {
+    top: 0;
+  }
+
+  .mobile-icon:after,
+  .middle-line {
+    bottom: 0;
+  }
+
+  .mobile-icon:before {
+    width: 66%;
+  }
+
+  .mobile-icon:after {
+    width: 33%;
+  }
+
+  .middle-line {
+    margin: auto;
+  }
+
+  .mobile-icon:hover:before,
+  .mobile-icon:hover:after,
+  .mobile-icon.active:before,
+  .mobile-icon.active:after,
+  .mobile-icon.active .middle-line {
+    width: 100%;
+  }
+
+  .mobile-icon.active:before,
+  .mobile-icon.active:after {
+    top: 50%;
+    transform: rotate(-45deg);
+  }
+
+  .mobile-icon.active .middle-line {
+    transform: rotate(45deg);
+  }
+
+  .navbar-list {
+    display: none;
+    width: 100%;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0 40px;
+  }
+
+  .navbar-list.mobile {
+    background-color: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    display: block;
+    height: calc(100% - 45px);
+    bottom: 0;
+    left: 0;
+  }
+
+  .navbar-list li {
+    list-style-type: none;
+    position: relative;
+  }
+
+  .navbar-list li:before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #424245;
+  }
+
+  .navbar-list a {
+    color: #fff;
+    text-decoration: none;
+    display: flex;
+    height: 45px;
+    align-items: center;
+    padding: 0 10px;
+    font-size: 13px;
+  }
+
+  @media only screen and (min-width: 767px) {
+    .mobile-icon {
+      display: none;
+    }
+
+    .navbar-list {
+      display: flex;
+      padding: 0;
+    }
+
+    .navbar-list a {
+      display: inline-flex;
+    }
+  }
 
   @media (min-width: 640px) {
     main {
@@ -102,22 +228,26 @@
   .button-container {
     background-color: #262626;
   }
+  nav {
+    background-color: rgba(0, 0, 0, 0.8);
+    font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    height: 45px;
+  }
 
 </style>
 
 
 <script lang="ts" type="module">
 
-    import type { Cat10 } from "../electron/cat10/Cat10";
-    import type { Cat21 } from "../electron/cat21/Cat21";
-    import { initIpcMainBidirectional, ipcMainBidirectional } from "./ipcMain/ipcMainCallers";
-    import { parseIpcMainReceiveMessage } from "./ipcMain/ipcMainReceiverParser";
-    import Simulation from "./components/simulation.svelte"
-    import { initializeMap } from "./arcgis/map";
-    import GenericProps from "./items/GenericProps.svelte";
+  import type { Cat10 } from "../electron/cat10/Cat10";
+  import type { Cat21 } from "../electron/cat21/Cat21";
+  import { initIpcMainBidirectional, ipcMainBidirectional } from "./ipcMain/ipcMainCallers";
+  import { parseIpcMainReceiveMessage } from "./ipcMain/ipcMainReceiverParser";
+  import Simulation from "./components/simulation.svelte"
+  import { initializeMap } from "./arcgis/map";
+  import GenericProps from "./items/GenericProps.svelte";
+  import { onMount } from "svelte";
 
-
-  
   let messages: ( Cat10 | Cat21) [] = [];
   let numberOfMsg = 0;
   
@@ -127,17 +257,16 @@
   let selectedCat: string;
   let selectedInstr: string;
   let searchBox = "";
-  let searchPicker = "Any";
+  let searchPicker = "Filter";
 
   let simulation : Simulation;
-  let visibleItem = "TABLE";
+  let visibleItem = "MAP";
 
   let selectedRow: number | null = null;
   let allChildComponents = new Map<number, GenericProps>();
   let allChildComponentsKeys = Array.from(allChildComponents.keys());
 
   async function handleLoadSomeMsgs() {
-    visibleItem = "TABLE";
     numberOfMsg = Number.parseInt(await initIpcMainBidirectional("file-picker"));
     if (!numberOfMsg) return;
     messages = [];
@@ -168,7 +297,7 @@
       Instrument: [],
     };
     let search = searchBox;
-    if (searchPicker !== "Any") {
+    if (searchPicker !== "Filter") {
       search = "";
       if (searchPicker === "Target Address") {
         filter.TargetAddress = searchBox;
@@ -260,20 +389,62 @@
       }
     }
   }
-  function handleNavClick(event) {
-    // Handle navbar click events here
-    console.log(event.target.innerText);
-  }
+  let showMobileMenu = false;
+
+  const navItems = [
+  { label: "MAP", href: "#", clickHandler: handleMapClick},
+  { label: "TABLE", href: "#", clickHandler: handleTableClick},
+  { label: "FILE", href: "#", clickHandler: handleLoadSomeMsgs }
+  ];
+
+  
+  let selectedItem = navItems[0];
+
+  
+  // Mobile menu click event handler
+  const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
+
+  const handleNavItemClick = (item:any) => {
+    selectedItem = item;
+    // Add your custom logic for handling the selected item
+    console.log("Selected item:", item.label);
+  };
+
+
+  // Media match query handler
+  const mediaQueryHandler = (e: { matches: any; }) => {
+    // Reset mobile state
+    if (!e.matches) {
+      showMobileMenu = false;
+    }
+  };
+
+  // Attach media query listener on mount hook
+  onMount(() => {
+    const mediaListener = window.matchMedia("(max-width: 767px)");
+
+    mediaListener.addListener(mediaQueryHandler);
+  });
   
 </script>
-  <button type="button" class="btn btn-primary simulation-button" on:click="{handleMapClick}"
-  >Simulation</button>
-  <button type="button" class="table-button" on:click="{handleTableClick}"
-      >Table  </button> 
-
-
-<button type="button" class="btn btn-primary file-button" on:click="{handleLoadSomeMsgs}"
-      >File  <i class="bi bi-folder2-open"></i></button> 
+<nav>
+  <div class="inner">
+    <div on:click={handleMobileIconClick} class={`mobile-icon${showMobileMenu ? ' active' : ''}`}>
+      <div class="middle-line"></div>
+    </div>
+    <ul class={`navbar-list${showMobileMenu ? ' mobile' : ''}`}>
+      <li>
+        <a href={'#'} on:click="{handleMapClick}"> MAP </a>
+      </li>
+      <li>
+        <a href={'#'} on:click="{handleTableClick}"> TABLE </a>
+      </li>
+      <li>
+        <a href={'#'} on:click="{handleLoadSomeMsgs}"> FILE </a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 <main>
   <div class="{visibleItem === 'MAP' ? 'main overflow' : 'main'}">
@@ -306,7 +477,7 @@
             bind:value="{searchPicker}"
             aria-label="Example select with button addon"
           >
-            <option selected>Any</option>
+            <option selected>Filter</option>
             <option>Target Address</option>
             <option>Target identification</option>
           </select>
